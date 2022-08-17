e8488d6f48bb1019e0de46f56ae981255bd4ce2d
tab1tha <tambetachere@gmail.com>
2022-08-17 11:33:23 +0100

    increment counter at start

---
+++ track_everything.py
@@ -0,0 +1,19 @@
+# This file keeps track of the sample output produced by key changes to parts of the tool.
+
+# assigning count IDs in CINxml_to_csv
+"""
+***********child {'child_count': 1}*************
+--------('ChildIdentifiers', {'child_count': 1, 'ChildIdentifiersCount': 1})-------------
+--------('ChildCharacteristics', {'child_count': 1, 'ChildCharacteristicsCount': 1})-------------
+--------('CINdetails', {'child_count': 1, 'CINdetailsCount': 1})-------------
+Assessments:{'child_count': 1, 'CINdetailsCount': 1, 'AssessmentsCount': 1}
+FactorsIdentifiedAtAssessment:{'child_count': 1, 'CINdetailsCount': 1, 'AssessmentsCount': 1, 'FactorsIdentifiedAtAssessmentCount': 1}     
+FactorsIdentifiedAtAssessment:{'child_count': 1, 'CINdetailsCount': 1, 'AssessmentsCount': 1, 'FactorsIdentifiedAtAssessmentCount': 2}     
+FactorsIdentifiedAtAssessment:{'child_count': 1, 'CINdetailsCount': 1, 'AssessmentsCount': 1, 'FactorsIdentifiedAtAssessmentCount': 3}     
+Assessments:{'child_count': 1, 'CINdetailsCount': 1, 'AssessmentsCount': 2}
+FactorsIdentifiedAtAssessment:{'child_count': 1, 'CINdetailsCount': 1, 'AssessmentsCount': 2, 'FactorsIdentifiedAtAssessmentCount': 1}     
+CINPlanDates:{'child_count': 1, 'CINdetailsCount': 1, 'CINPlanDatesCount': 1}
+Section47:{'child_count': 1, 'CINdetailsCount': 1, 'Section47Count': 1}
+ChildProtectionPlans:{'child_count': 1, 'CINdetailsCount': 1, 'ChildProtectionPlansCount': 1}
+Reviews:{'child_count': 1, 'CINdetailsCount': 1, 'ChildProtectionPlansCount': 1, 'ReviewsCount': 1}
+"""
