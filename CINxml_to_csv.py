ee50d382ff169ad1002b8c40adddead2393e8e85
tab1tha <tambetachere@gmail.com>
2022-08-17 12:10:39 +0100

    black format

--- CINxml_to_csv.py
+++ CINxml_to_csv.py
@@ -1,22 +1,27 @@
 import xml.etree.ElementTree as ET
 
 # parse() processes files while fromstring() processes strings
-fulltree = ET.parse('CIN_Census_2021.xml')
-#fulltree = ET.parse('fake_CIN_data.xml')
+fulltree = ET.parse("CIN_Census_2021.xml")
+# fulltree = ET.parse('fake_CIN_data.xml')
+
 # Get the outermost tag as a starting point
 message = fulltree.getroot()
 # the components are [Header, Children]
 children = message[1]
 
-child_count = 0 
+child_count = 0
 for child in children:
     child_count += 1
     child.set("child_count", child_count)
     print(f"***********child {child.attrib}*************")
-    
+
     # TODO check that the unique groups never have a count>0. That is ChildIdentifiers and ChildCharacteristics.
-    group_counts = {"ChildIdentifiersCount":0, "ChildCharacteristicsCount":0, "CINdetailsCount":0}
-    for group in child: 
+    group_counts = {
+        "ChildIdentifiersCount": 0,
+        "ChildCharacteristicsCount": 0,
+        "CINdetailsCount": 0,
+    }
+    for group in child:
         # groups are: ChildCharacteristics, ChildIdentifiers, CINdetails
         group.set("child_count", child_count)
         group_count = group.tag + "Count"
@@ -24,9 +29,14 @@ for child in children:
 
         group.set(group_count, group_counts[group_count])
         print(f"--------{group.tag, group.attrib}-------------")
-        
-        element_counts = {"AssessmentsCount":0, "CINPlanDatesCount":0, "Section47Count":0, "ChildProtectionPlansCount":0}
-        for element in group: 
+
+        element_counts = {
+            "AssessmentsCount": 0,
+            "CINPlanDatesCount": 0,
+            "Section47Count": 0,
+            "ChildProtectionPlansCount": 0,
+        }
+        for element in group:
             # example elements are: Assessments, CINPlanDates, Section47, ChildProtectionPlans
             element_count = element.tag + "Count"
             if element_count in element_counts:
@@ -37,8 +47,11 @@ for child in children:
                 print(f"{element.tag}:{element.attrib}")
 
                 if element.tag in ["Assessments", "ChildProtectionPlans"]:
-                    sub_counts = {"ReviewsCount":0, "FactorsIdentifiedAtAssessmentCount":0}
-                    for sub in element:                        
+                    sub_counts = {
+                        "ReviewsCount": 0,
+                        "FactorsIdentifiedAtAssessmentCount": 0,
+                    }
+                    for sub in element:
                         sub_count = sub.tag + "Count"
                         if sub_count in sub_counts:
                             sub_counts[sub_count] += 1
@@ -47,4 +60,4 @@ for child in children:
                             sub.set(group_count, group_counts[group_count])
                             sub.set(element_count, element_counts[element_count])
                             sub.set(sub_count, sub_counts[sub_count])
-                            print(f"{sub.tag}:{sub.attrib}")                       
+                            print(f"{sub.tag}:{sub.attrib}")
--- create_CIN_issues.py
+++ create_CIN_issues.py
@@ -12,13 +12,13 @@ token = os.getenv("GITHUB_TOKEN", "write_your_token_here_keep_quotes")
 # initialise the Github class
 gh = Github(token)
 # replace content of bracket with the path of the repo you want to push to.
-repo = gh.get_repo('SocialFinanceDigitalLabs/CIN-validator')
+repo = gh.get_repo("SocialFinanceDigitalLabs/CIN-validator")
 
 # get rules file and confirm it's type.
-rules_file = sys.argv[1] 
-while not rules_file.endswith('xlsx'):           
+rules_file = sys.argv[1]
+while not rules_file.endswith("xlsx"):
     rules_file = input("Enter the name of the validation file, incl extension: ")
-## Read validation rules into a dataframe.       
+## Read validation rules into a dataframe.
 df = pd.read_excel(rules_file, sheet_name=1)
 
 """
@@ -27,26 +27,30 @@ Continue from where you ended as shown. Start from the next index row (e.g row 9
 new_df = df.loc[92:, :].copy()
 """
 # escape special characters so that they are not interpreted as Github markdown.
-df['Validation check'] = df['Validation check'].str.replace('<', '\<').str.replace('>', '\>')
+df["Validation check"] = (
+    df["Validation check"].str.replace("<", "\<").str.replace(">", "\>")
+)
 
-# loop over dataframe 
+# loop over dataframe
 for index, row in df.iterrows():
-    print(row['Sequence number'])
+    print(row["Sequence number"])
     # describe the properties of the github issue to be created at each row.
     repo.create_issue(
         # title should be of the form --> rule number: rule description
-        title = f"Rule {row['Sequence number']} : {row['Message']}",
+        title=f"Rule {row['Sequence number']} : {row['Message']}",
         # body is the text that includes the pseudocode of the rule
-        body = row['Validation check'],
+        body=row["Validation check"],
         # labels are any distinctive tags that might be useful during development.
-        labels = [row['Error/ query'],]
-        #labels = [row['Error/ query'], row['Module']]
-        """If label names frequently trigger validation errors, skip the labels part and create them using Github's UI"""
+        labels=[
+            row["Error/ query"],
+        ]
+        # labels = [row['Error/ query'], row['Module']]
+        ## If label names frequently trigger validation errors, skip the labels part and create them using Github's UI
     )
     # wait between runs in order not to exceed Github's secondary rate limit.
     time.sleep(2)
- 
-## How to run this file. 
+
+## How to run this file.
 """
 In your command line, do:
 python create_CIN_issues.py rules_file_name_here
