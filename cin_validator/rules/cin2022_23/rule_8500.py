bf742ed4145d548b55179c972cde61deea1695a4
tab1tha <tambetachere@gmail.com>
2022-09-15 18:15:42 +0100

    adds template instructions to rule 8500

--- cin_validator/rules/cin2022_23/rule_8500.py
+++ cin_validator/rules/cin2022_23/rule_8500.py
@@ -6,33 +6,43 @@ from cin_validator.rule_engine import rule_definition, CINTable, RuleContext
 from cin_validator.rule_engine import IssueLocator
 from cin_validator.test_engine import run_rule
 
+# Get tables and columns of interest from the CINTable object defined in rule_engine/__api.py
+# Replace ChildIdentifiers with the table name, and LAChildID with the column name you want.
+
 ChildIdentifiers = CINTable.ChildIdentifiers
 LAchildID = ChildIdentifiers.LAchildID
 
-
+# define characteristics of rule
 @rule_definition(
+    # write the rule code here, in place of 8500
     code=8500,
+    # replace ChildIdentifiers with the value in the module column of the excel sheet corresponding to this rule .
     module=CINTable.ChildIdentifiers,
+    # replace the message with the corresponding value for this rule, gotten from the excel sheet.
     message="LA Child ID missing",
+    # The column names tend to be the words within the < > signs in the github issue description.
     affected_fields=[LAchildID],
 )
 def validate(
     data_container: Mapping[CINTable, pd.DataFrame], rule_context: RuleContext
 ):
-
+    # Replace ChildIdentifiers with the name of the table you need.
     df = data_container[ChildIdentifiers]
 
-    # select all the locations where the child ID is absent
+    # implement rule logic as descriped by the Github issue. Put the description as a comment above the implementation as shown.
+
+    # Where present, the <CPPStartDate> (N00105) must be on or after the <CINReferralDate> (N00100)
     failing_indices = df[df[LAchildID].isna()].index
 
-    # We can push the entire index to keep code small and memory usage low
+    # Replace ChildIdentifiers and LAchildID with the table and column name concerned in your rule, respectively. 
+    # If there are multiple columns or table, make this sentence multiple times.
     rule_context.push_issue(
         table=ChildIdentifiers, field=LAchildID, row=failing_indices
     )
 
 
 def test_validate():
-    # Create some sample data
+    # Create some sample data such that some values pass the validation and some fail.
     child_identifiers = pd.DataFrame([[1234], [pd.NA], [pd.NA]], columns=[LAchildID])
 
     # Run rule function passing in our sample data
@@ -40,12 +50,17 @@ def test_validate():
 
     # The result contains a list of issues encountered
     issues = list(result.issues)
+    # replace 2 with the number of failing points you expect from the sample data.
     assert len(issues) == 2
+    # replace the table and column name as done earlier. 
+    # The last numbers represent the index values where you expect the sample data to fail the validation check.
     assert issues == [
         IssueLocator(CINTable.ChildIdentifiers, LAchildID, 1),
         IssueLocator(CINTable.ChildIdentifiers, LAchildID, 2),
     ]
 
-    # As well as the rule definition
+    # Check that the rule definition is what you wrote in the context above.
+
+    # replace 8500 with the rule code and put the appropriate message in its place too.
     assert result.definition.code == 8500
     assert result.definition.message == "LA Child ID missing"
