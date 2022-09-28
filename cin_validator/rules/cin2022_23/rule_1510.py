2d91d5b5ccbd0ff08dc38d5eb81c1544d42ebb6d
Phil Deakin <89073292+PhilDeakin@users.noreply.github.com>
2022-09-28 14:57:14 +0100

    Update rule_1510.py

--- cin_validator/rules/cin2022_23/rule_1510.py
+++ cin_validator/rules/cin2022_23/rule_1510.py
@@ -55,7 +55,7 @@ def validate(
     df2['LAST_C'] = df2['LAST_C'].apply(lambda x: int(x) if str(x).isdigit() else pd.NA)
     df2 = df2[df2['LAST_C'].notna()]
     for i in range(1,13):
-        df2['C' + str(i)] = (df['UPN'].str[i].astype(int)**(i+1)) % 23      #Largest possible value before mod 23 is applied is 9^13 can Python handle that as an int??
+        df2['C' + str(i)] = (df['UPN'].str[i].astype(int) * (i+1)) % 23
     df2['SUMMED'] = df2[['C1','C2','C3','C4','C5','C6','C7','C8','C9','C10','C11','C12']].sum(axis=1) % 23
 
     check_map = enumerate(list('ABCDEFGHJKLMNPQRTUVWX'))
