eb0454fba8ce45358ee48b8264d618b34ded5091
Phil Deakin <89073292+PhilDeakin@users.noreply.github.com>
2022-09-30 19:54:46 +0100

    Update rule_1510.py

--- cin_validator/rules/cin2022_23/rule_1510.py
+++ cin_validator/rules/cin2022_23/rule_1510.py
@@ -54,9 +54,9 @@ def validate(
     df2['LAST_C'] = df2['UPN'].str[1: ]
     df2['LAST_C'] = df2['LAST_C'].apply(lambda x: int(x) if str(x).isdigit() else pd.NA)
     df2 = df2[df2['LAST_C'].notna()]
+    df2['SUMMED'] = 0
     for i in range(1,13):
-        df2['C' + str(i)] = (df2['UPN'].str[i].astype(int) * (i+1)) % 23
-    df2['SUMMED'] = df2[['C1','C2','C3','C4','C5','C6','C7','C8','C9','C10','C11','C12']].sum(axis=1) % 23
+        df2['SUMMED'] = (df2['SUMMED'] + (df2['UPN'].str[i].astype(int) * (i+1))) % 23
 
     check_map = enumerate(list('ABCDEFGHJKLMNPQRTUVWX'))
     check_map = dict((i, j) for i, j in check_map)
