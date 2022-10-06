ca535320707edb2c1ba2e384da6063d0387e4d93
tab1tha <tambetachere@gmail.com>
2022-10-06 13:29:41 +0100

    light formatting

--- cin_validator/rules/cin2022_23/rule_1540.py
+++ cin_validator/rules/cin2022_23/rule_1540.py
@@ -28,34 +28,32 @@ def validate(
 ):
     # Replace ChildIdentifiers with the name of the table you need.
     df = data_container[ChildIdentifiers]
-    
+
     # implement rule logic as descriped by the Github issue. Put the description as a comment above the implementation as shown.
 
     # If <UPN> (N00001) present Characters 5-12 of <UPN> must be numeric
-    
+
     #  df takes a slice of rows of df where the UPN column doesn't have Na/NaN values
-    df = df.loc[df['UPN'].notna()]
+    df = df.loc[df["UPN"].notna()]
+    """
+    Returns indices of rows where character 5:12 of UPN contains non numerical characters.
+    Does this by:
+    Returning a boolean for the logic check to see is characters 5:12 contain only numerical characters.
+    Using the not operator (~) to return values as false where the logic returns true (and true if there are non-numeric characters).
+    Slicing df according to this criteria. 
+    Returns indices of the rows of this df to failing_indices."""
+    failing_indices = df[~df["UPN"].str[4:12].str.isdigit()].index
 
-    #  Returns indices of rows where character 5:12 of UPN contains non numerical characters.
-    #  Does this by:
-    #  Returning a boolean for the logic check to see is characters 5:12 contain only numerical characters.
-    #  Using the not operator (~) to return values as false where the logic returns true (and true if there are non-numeric characters).
-    #  Slicing df according to this criteria. 
-    #  Returns indices of the rows of this df to failing_indices.
-    failing_indices = df[~df['UPN'].str[4:12].str.isdigit()].index
-
-    # Replace ChildIdentifiers and LAchildID with the table and column name concerned in your rule, respectively. 
+    # Replace ChildIdentifiers and LAchildID with the table and column name concerned in your rule, respectively.
     # If there are multiple columns or table, make this sentence multiple times.
-    rule_context.push_issue(
-        table=ChildIdentifiers, field=UPN, row=failing_indices
-    )
+    rule_context.push_issue(table=ChildIdentifiers, field=UPN, row=failing_indices)
 
 
 def test_validate():
     # Create some sample data such that some values pass the validation and some fail.
-    child_identifiers =  pd.DataFrame(
-        {'UPN' : [pd.NA, 'X000000000000', 'X0000y0000000', 'x0000000er00e0']
-        })
+    child_identifiers = pd.DataFrame(
+        {"UPN": [pd.NA, "X000000000000", "X0000y0000000", "x0000000er00e0"]}
+    )
 
     # Run rule function passing in our sample data
     result = run_rule(validate, {ChildIdentifiers: child_identifiers})
@@ -64,7 +62,7 @@ def test_validate():
     issues = list(result.issues)
     # replace 2 with the number of failing points you expect from the sample data.
     assert len(issues) == 2
-    # replace the table and column name as done earlier. 
+    # replace the table and column name as done earlier.
     # The last numbers represent the index values where you expect the sample data to fail the validation check.
     assert issues == [
         IssueLocator(CINTable.ChildIdentifiers, UPN, 2),
