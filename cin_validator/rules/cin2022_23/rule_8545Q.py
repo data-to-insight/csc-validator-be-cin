3519bc915e7f4fe692493e166e496b162996b4da
tab1tha <tambetachere@gmail.com>
2022-10-20 09:38:39 +0100

    generalise the process of using period_of_census

--- cin_validator/rules/cin2022_23/rule_8545Q.py
+++ cin_validator/rules/cin2022_23/rule_8545Q.py
@@ -2,18 +2,26 @@ from typing import Mapping
 
 import pandas as pd
 
-from cin_validator.rule_engine import rule_definition, CINTable, RuleContext
-from cin_validator.rule_engine import IssueLocator
+from cin_validator.rule_engine import (
+    CINTable,
+    IssueLocator,
+    RuleContext,
+    RuleType,
+    rule_definition,
+)
 from cin_validator.test_engine import run_rule
+from cin_validator.utils import make_census_period
 
 ChildIdentifiers = CINTable.ChildIdentifiers
 Header = CINTable.Header
 PersonDeathDate = ChildIdentifiers.PersonDeathDate
+Year = Header.Year
 
 # define characteristics of rule
 @rule_definition(
     code="8545Q",
     module=CINTable.ChildIdentifiers,
+    rule_type=RuleType.QUERY,
     message="Please check: Child's date of death should be within the census year",
     affected_fields=[PersonDeathDate],
 )
@@ -22,28 +30,34 @@ def validate(
 ):
     df = data_container[ChildIdentifiers]
     df_ref = data_container[Header]
- 
+
     """
     If present, <PersonDeathDate> (N00108) must be within [Period_of_Census]
     """
 
-    df = df[["PersonDeathDate"]]
-    df["PersonDeathDate"] = pd.to_datetime(df["PersonDeathDate"], format=r"%Y-%m-%d", errors="coerce")
-    #Death date must not be null, invalid text dates are made null in the line above
-    df = df[df["PersonDeathDate"].notna()]
-    #Create a key column to join
-    df["Key"] = 0
+    df = df[[PersonDeathDate]]
 
-    df_ref = df_ref[["ReferenceDate"]]
+    # Death date must not be null, invalid text dates are made null in the line above
+    df = df[df[PersonDeathDate].notna()]
+
+    # Create a key column to join
+    # df["Key"] = 0
+    """df_ref = df_ref[[ReferenceDate]]
     df_ref["YearEnd"] = pd.to_datetime(df_ref["ReferenceDate"], format=r"%Y-%m-%d", errors="coerce") #equals 31/03/2023  (for example, year can be different)
     df_ref["YearStart"] = df_ref["YearEnd"] - pd.DateOffset(years=1) + pd.DateOffset(days=1)         #equals 01/04/2022  (for example, year can be different)
     #Create a key column to join
-    df_ref["Key"] = 0
+    df_ref["Key"] = 0"""
+    # df = df.merge(df_ref, on="Key")
 
-    df = df.merge(df_ref, on="Key")
-
-    #DeathDate isn't in the financial year
-    df = df[~((df["PersonDeathDate"] >= df["YearStart"]) & (df["PersonDeathDate"] <= df["YearEnd"]))]
+    collection_year = df_ref[Year]
+    collection_start, collection_end = make_census_period(collection_year)
+    # DeathDate isn't in the financial year
+    df = df[
+        ~(
+            (df[PersonDeathDate] >= collection_start)
+            & (df[PersonDeathDate] <= collection_end)
+        )
+    ]
 
     failing_indices = df.index
 
@@ -54,10 +68,24 @@ def validate(
 
 def test_validate():
 
-    PDeath = ["2022-04-25", "2022-03-01", "2022-12-25", "2021-04-27", "2021-11-21", "2021-08-20", "2020-04-17", "1666-55-55", pd.NA]
+    p_death = pd.to_datetime(
+        [
+            "2022-04-25",
+            "2022-03-01",
+            "2022-12-25",
+            "2021-04-27",
+            "2021-11-21",
+            "2021-08-20",
+            "2020-04-17",
+            "1666-55-55",
+            pd.NA,
+        ],
+        format="%Y/%m/%d",
+        errors="coerce",
+    )
 
-    fake_ident = pd.DataFrame({"PersonDeathDate": PDeath})
-    fake_head = pd.DataFrame({"ReferenceDate": ["2022-03-31"]})
+    fake_ident = pd.DataFrame({PersonDeathDate: p_death})
+    fake_head = pd.DataFrame({Year: ["2022"]})
 
     result = run_rule(validate, {ChildIdentifiers: fake_ident, Header: fake_head})
 
