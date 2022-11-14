ff9378ad105858beb3cd5c81aee899fa1f0509e6
GBurrel <113117252+GBurrel@users.noreply.github.com>
2022-11-14 08:24:38 +0000

    Draft Code

---
+++ cin_validator/rules/cin2022_23/rule_8920.py
@@ -0,0 +1,277 @@
+from typing import Mapping
+
+import pandas as pd
+
+from cin_validator.rule_engine import CINTable, RuleContext, rule_definition
+from cin_validator.test_engine import run_rule
+from cin_validator.utils import make_census_period
+
+# Get tables and columns of interest from the CINTable object defined in rule_engine/__api.py
+
+ChildProtectionPlans = CINTable.ChildProtectionPlans
+CPPendDate = ChildProtectionPlans.CPPendDate
+LAchildID = ChildProtectionPlans.LAchildID
+#CPP_Planid = ChildProtectionPlans.LAchildID
+
+ChildIdentifiers = CINTable.ChildIdentifiers
+PersonDeathDate = ChildIdentifiers.PersonDeathDate
+LAchildID = ChildIdentifiers.LAchildID
+
+# define characteristics of rule
+@rule_definition(
+    # write the rule code here
+    code=8920,
+    # replace ChildProtectionPlans with the value in the module column of the excel sheet corresponding to this rule .
+    # Note that even if multiple tables are involved, one table will be named in the module column.
+    module=CINTable.ChildProtectionPlans,
+    # replace the message with the corresponding value for this rule, gotten from the excel sheet.
+    message="Child Protection Plan cannot end after the child’s Date of Death",
+    # The column names tend to be the words within the < > signs in the github issue description.
+    affected_fields=[
+        PersonDeathDate,
+        CPPendDate,
+    ],
+)
+def validate(
+    data_container: Mapping[CINTable, pd.DataFrame], rule_context: RuleContext
+):
+    # PREPARING DATA
+
+    # Replace ChildProtectionPlans with the name of the table you need.
+    df_cpp = data_container[ChildProtectionPlans].copy()
+    df_child = data_container[ChildIdentifiers].copy()
+
+    # Before you begin, rename the index so that the initial row positions can be kept intact.
+    df_cpp.index.name = "ROW_ID"
+    df_child.index.name = "ROW_ID"
+
+    # Resetting the index causes the ROW_IDs to become columns of their respective DataFrames
+    # so that they can come along when the merge is done.
+    df_cpp.reset_index(inplace=True)
+    df_child.reset_index(inplace=True)
+
+    # lOGIC
+    # Implement rule logic as described by the Github issue.
+    # Put the description as a comment above the implementation as shown.
+
+    # Within a <ChildProtectionPlans> group, there should be no <CPPreviewDate> (N00116) that is the same as or before the <CPPstartDate> (N00105)
+    # Issues dfs should return rows where CPPreviewDate is less than or equal to the CPPstartDate
+
+    #  Create dataframes which only have rows with CP plans, and which should have one plan per row.
+    df_cpp = df_cpp[df_cpp[CPPendDate].notna()]
+    df_child = df_child[df_child[PersonDeathDate].notna()]
+
+    #  Merge tables to get corresponding CP plan group and reviews
+    df_merged = df_cpp.merge(
+        df_child,
+        left_on=["LAchildID", "CINdetailsID"],
+        right_on=["LAchildID", "CINdetailsID"],
+        how="left",
+        suffixes=("_cpp", "_reviews"),
+    )
+
+    #  Get rows where CPPendDate is on or before PersonDeathDate
+    condition = df_merged[CPPendDate] >= df_merged[PersonDeathDate]
+    df_merged = df_merged[condition].reset_index()
+
+    # create an identifier for each error instance.
+    # In this case, the rule is checked for each CPPstartDate, in each CPplanDates group (differentiated by CP dates), in each child (differentiated by LAchildID)
+    # So, a combination of LAchildID, CPPstartDate and CPPreviewDate identifies and error instance.
+    df_merged["ERROR_ID"] = tuple(
+        zip(df_merged[LAchildID], df_merged[CPPendDate], df_merged[PersonDeathDate])
+    )
+
+    # The merges were done on copies of cpp_df and reviews_df so that the column names in dataframes themselves aren't affected by the suffixes.
+    # we can now map the suffixes columns to their corresponding source tables such that the failing ROW_IDs and ERROR_IDs exist per table.
+    df_cpp_issues = (
+        df_cpp.merge(df_merged, left_on="ROW_ID", right_on="ROW_ID_cpp")
+        .groupby("ERROR_ID")["ROW_ID"]
+        .apply(list)
+        .reset_index()
+    )
+    df_reviews_issues = (
+        df_child.merge(df_merged, left_on="ROW_ID", right_on="ROW_ID_reviews")
+        .groupby("ERROR_ID")["ROW_ID"]
+        .apply(list)
+        .reset_index()
+    )
+
+    # Ensure that you maintain the ROW_ID, and ERROR_ID column names which are shown above. They are keywords in this project.
+    rule_context.push_type_2(
+        table=ChildProtectionPlans, columns=[CPPendDate], row_df=df_cpp_issues
+    )
+    rule_context.push_type_2(
+        table=ChildIdentifiers, columns=[PersonDeathDate], row_df=df_reviews_issues
+    )
+
+def test_validate():
+    # Create some sample data such that some values pass the validation and some fail.
+    sample_cpp = pd.DataFrame(
+        [
+            {
+                "LAchildID": "child1",
+                "CINdetailsID": "CDID1",
+                "CPPendDate": "26/05/2000",  # Fails as dates are the same
+            },
+            {
+                "LAchildID": "child2",
+                "CINdetailsID": "CDID2",
+                "CPPendDate": "27/06/2002",  #  Fails, review (26/5/2000) before start
+            },
+            {
+                "LAchildID": "child3",
+                "CINdetailsID": "CDID3",
+                "CPPendDate": "07/02/2001",  # Fails as review is before start (26/5/2000)
+            },
+            {
+                "LAchildID": "child4",
+                "CINdetailsID": "CDID4",
+                "CPPstartDate": "26/05/2000",  # Passes as Start is before Review (30/05/2000)
+            },
+            {
+                "LAchildID": "child5",
+                "CINdetailsID": "CDID5",
+                "CPPendDate": "26/05/2000",  # Passes
+            },
+            {
+                "LAchildID": "child6",
+                "CINdetailsID": "CDID6",
+                "CPPendDate": pd.NA,  # Ignored as rows with no start and end are dropped (this is picked up by other rules)
+            },
+            {
+                "LAchildID": "child7",
+                "CINdetailsID": "CDID7",
+                "CPPendDate": "14/03/2001",  # Ignored as there is no review date
+            },
+        ]
+    )
+    sample_children = pd.DataFrame(
+        [
+            {
+                "LAchildID": "child1",  # Fails as Death Date is before CPP end date
+                "CINdetailsID": "CDID1",
+                "PersonDeathDate": "25/05/2000",
+            },
+            {
+                "LAchildID": "child2",  # Fails
+                "CINdetailsID": "CDID2",
+                "PersonDeathDate": "29/05/2000",
+            },
+            {
+                "LAchildID": "child3",  # Fails
+                "CINdetailsID": "CDID3",
+                "PersonDeathDate": "26/03/2000",
+            },
+            {
+                "LAchildID": "child4",
+                "CINdetailsID": "CDID4",
+                "PersonDeathDate": "30/05/2000",
+            },
+            {
+                "LAchildID": "child5",
+                "CINdetailsID": "CDID5",
+                "PersonDeathDate": "27/05/2000",
+            },
+            {
+                "LAchildID": "child6",
+                "CINdetailsID": "CDID6",
+                "PersonDeathDate": "26/05/2000",
+            },
+            {
+                "LAchildID": "child7",
+                "CINdetailsID": "CDID7",
+                "PersonDeathDate": pd.NA,
+            },
+        ]
+    )
+
+    # If rule requires columns containing date values, convert those columns to datetime objects first. Do it here in the test_validate function, not above.
+    sample_cpp[CPPendDate] = pd.to_datetime(
+        sample_cpp[CPPendDate], format="%d/%m/%Y", errors="coerce"
+    )
+    sample_children["PersonDeathDate"] = pd.to_datetime(
+        sample_children["PersonDeathDate"], format="%d/%m/%Y", errors="coerce"
+    )
+
+    # Run the rule function, passing in our sample data.
+    result = run_rule(
+        validate,
+        {
+            ChildProtectionPlans: sample_cpp,
+            ChildIdentifiers: sample_children,
+        },
+    )
+
+    # Use .type2_issues to check for the result of .push_type2_issues() which you used above.
+    issues_list = result.type2_issues
+    assert len(issues_list) == 3
+    # the function returns a list on NamedTuples where each NamedTuple contains (table, column_list, df_issues)
+    # pick any table and check it's values. the tuple in location 1 will contain the Reviews columns because that's the second thing pushed above.
+    issues = issues_list[1]
+
+    # get table name and check it. Replace Reviews with the name of your table.
+    issue_table = issues.table
+    assert issue_table == ChildIdentifiers
+
+    # check that the right columns were returned. Replace CPPreviewDate  with a list of your columns.
+    issue_columns = issues.columns
+    assert issue_columns == [CPPendDate]
+
+    # check that the location linking dataframe was formed properly.
+    issue_rows = issues.row_df
+    # replace 3 with the number of failing points you expect from the sample data.
+    assert len(issue_rows) == 3
+    # check that the failing locations are contained in a DataFrame having the appropriate columns. These lines do not change.
+    assert isinstance(issue_rows, pd.DataFrame)
+    assert issue_rows.columns.to_list() == ["ERROR_ID", "ROW_ID"]
+
+    # Create the dataframe which you expect, based on the fake data you created. It should have two columns.
+    # - The first column is ERROR_ID which contains the unique combination that identifies each error instance, which you decided on, in your zip, earlier.
+    # - The second column in ROW_ID which contains a list of index positions that belong to each error instance.
+
+    # The ROW ID values represent the index positions where you expect the sample data to fail the validation check.
+    expected_df = pd.DataFrame(
+        [
+            {
+                "ERROR_ID": (
+                    "child1",  # ChildID
+                    # CPP End Date
+                    pd.to_datetime("26/05/2000", format="%d/%m/%Y", errors="coerce"),
+                    # Person Death Date
+                    pd.to_datetime("25/05/2000", format="%d/%m/%Y", errors="coerce"),
+                ),
+                "ROW_ID": [0],
+            },
+            {
+                "ERROR_ID": (
+                    "child1",  # ChildID
+                    # CPP End Date
+                    pd.to_datetime("27/06/2002", format="%d/%m/%Y", errors="coerce"),
+                    # Person Death Date
+                    pd.to_datetime("29/05/2000", format="%d/%m/%Y", errors="coerce"),
+                ),
+                "ROW_ID": [1],
+            },
+            {
+                "ERROR_ID": (
+                    "child3",  # ChildID
+                    # CPP End Date
+                    pd.to_datetime("07/02/2001", format="%d/%m/%Y", errors="coerce"),
+                    # Person Death Date
+                    pd.to_datetime("26/03/2000", format="%d/%m/%Y", errors="coerce"),
+                ),
+                "ROW_ID": [2],
+            },
+        ]
+    )
+    assert issue_rows.equals(expected_df)
+
+    # Check that the rule definition is what you wrote in the context above.
+
+    # replace 2885 with the rule code and put the appropriate message in its place too.
+    assert result.definition.code == 8920
+    assert (
+        result.definition.message
+        == "Child Protection Plan cannot end after the child’s Date of Death"
+    )
+
