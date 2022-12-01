94862755680d16a33e796a702056cbe3882003c1
GBurrel <113117252+GBurrel@users.noreply.github.com>
2022-12-01 12:12:32 +0000

    Closes Rule 4010 #79

--- cin_validator/rules/cin2022_23/rule_4010.py
+++ cin_validator/rules/cin2022_23/rule_4010.py
@@ -30,45 +30,30 @@ ReferenceDate = Header.ReferenceDate
 def validate(
     data_container: Mapping[CINTable, pd.DataFrame], rule_context: RuleContext
 ):
-    # Replace ChildIdentifiers with the name of the table you need.
+    # Replace ChildIdentifiers with the name of the table you need as well as a DataFrane for the Header as we are using referencing the Census Period EndDate
     df = data_container[CINplanDates]
-    #print(df)
-
     df_ref = data_container[Header]
-
-    #print(df_ref)
-    # must not be null
-    #df = df[df[CINPlanStartDate].notna()]
 
     # Select out only the ReferenceDate column from the DataFrame
     ref_data_series = df_ref[ReferenceDate]
 
-    print(ref_data_series)
+    # Allocate to the start and end of the census period to collection_start and collection_end
     collection_start, collection_end = make_census_period(ref_data_series)
 
     # implement rule logic as described by the Github issue. Put the description as a comment above the implementation as shown.
-
     # Where a <CINPlanDates> module is present, <CINPlanStartDate> (N00689) must be present and on or before the <ReferenceDate> (N00603)
     
-    df = df[df[CINPlanStartDate].notna()].index
+    # Check there is a value recorded for the CINPlanStartDate
+    df = df[df[CINPlanStartDate].notna()]
 
-    #failing_indices = df[
-    #    ~(df[CINPlanStartDate].isna())
-    #    | (collection_end < [CINPlanStartDate])
-    #].Index
+    # Where a CINPlanStartDate exists check to see if it is after the end of the Census Period (collection_end)
+    df = df[
+        (
+                (df[CINPlanStartDate] > collection_end)
+         )
+    ]
 
-    failing_indices = df[CINPlanStartDate > collection_end].index
-    #    ~(df[CINPlanStartDate].isna())
-    #    | (collection_end < [CINPlanStartDate])
-    #].Index
-
-    #print(failing_indices)
-   # failing_indices = df.index
-    #df = df(([CINPlanStartDate] > collection_end))
-
-    #failing_indices = df.index
-
-    #failing_indices = df[df[CINPlanStartDate] < collection_end ]
+    failing_indices = df.index
 
     # Replace ChildIdentifiers and LAchildID with the table and column name concerned in your rule, respectively.
     # If there are multiple columns or table, make this sentence multiple times.
@@ -79,50 +64,31 @@ def validate(
 def test_validate():
     # Create some sample data such that some values pass the validation and some fail.
     
-    fake_cin_start = pd.DataFrame(
+    cin_start = pd.to_datetime(
         [
-            {
-            CINPlanStartDate: "25/04/2022", #fail
-            },
-            {
-            CINPlanStartDate: "01/03/2022", #pass
-            },
-             {
-            CINPlanStartDate: "25/12/2022", #fail
-            },
-             {
-            CINPlanStartDate: "27/04/2021", #pass
-            } ,
-            {
-            CINPlanStartDate: "21/11/2021", #pass
-            },
-            {
-            CINPlanStartDate: "20/08/2021", #pass
-            },
-            {
-            CINPlanStartDate: "17/04/2020", #pass
-            },
-            {
-            CINPlanStartDate: pd.NA,
-            },
-        ]
+            
+            "25/04/2022", #fail (date is after the end of the census period end date of 31st March 2022)
+            "01/03/2022", #pass (date is before the end of the census period of 31st March 2022)
+            "25/12/2022", #fail (date is after the end of the census period end date of 31st March 2022)
+            "27/04/2021", #pass (date is before the end of the census period of 31st March 2022)
+            "21/11/2021", #pass (date is before the end of the census period of 31st March 2022)
+            "20/08/2021", #pass (date is before the end of the census period of 31st March 2022)
+            "17/04/2020", #pass (date is before the end of the census period of 31st March 2022)
+            pd.NA,
+        ],
+        format="%d/%m/%Y",
+        errors="coerce",
     )
-    fake_cin_start["CINPlanStartDate"] = pd.to_datetime(
-        fake_cin_start["CINPlanStartDate"], format="%d/%m/%Y", errors="coerce"
-    )
-        
-    #fake_cin_start = pd.DataFrame({CINplanDates: fake_cin_start})
-    fake_header = pd.DataFrame([{ReferenceDate: "31/03/2022"}])  # the census start date here will be 01/04/2021 
 
+    fake_cin_start = pd.DataFrame({CINPlanStartDate: cin_start})
+    fake_header = pd.DataFrame([{ReferenceDate: "31/03/2022"}])  # the census start date here will be 01/04/2021 
+    
     # Run rule function passing in our sample data
     result = run_rule(validate, {CINplanDates: fake_cin_start, Header: fake_header})
 
-    #print(result)
-    
     # The result contains a list of issues encountered
     issues = list(result.issues)
-
-        
+   
     # replace 2 with the number of failing points you expect from the sample data.
     assert len(issues) == 2
     # replace the table and column name as done earlier.
