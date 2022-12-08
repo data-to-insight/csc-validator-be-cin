51356d0d07d17e9ba8e25dea5232c0c8e835e2e4
William Levack Payne <108789355+willLPD2I@users.noreply.github.com>
2022-12-08 11:39:09 +0000

    black

--- cin_validator/rules/cin2022_23/rule_8842Q.py
+++ cin_validator/rules/cin2022_23/rule_8842Q.py
@@ -23,7 +23,7 @@ CPPreviewDate = Reviews.CPPreviewDate
     code="8842Q",
     # replace ChildIdentifiers with the value in the module column of the excel sheet corresponding to this rule .
     module=CINTable.Reviews,
-    rule_type = RuleType.QUERY,
+    rule_type=RuleType.QUERY,
     # replace the message with the corresponding value for this rule, gotten from the excel sheet.
     message="Please check: Review Record has a missing date",
     # The column names tend to be the words within the < > signs in the github issue description.
@@ -38,17 +38,15 @@ def validate(
     # implement rule logic as described by the Github issue. Put the description as a comment above the implementation as shown.
 
     # Where a <Reviews> group is present, a valid <CPPreviewdate> (N00116) should be present within the group
-    # Valid values for columns can be found in this document: 
+    # Valid values for columns can be found in this document:
     # https://assets.publishing.service.gov.uk/government/uploads/system/uploads/attachment_data/file/1025195/Children_in_need_census_2022_to_2023_guide.pdf
     condition = df[CPPreviewDate].isna()
-    
+
     failing_indices = df[condition].index
 
     # Replace ChildIdentifiers and LAchildID with the table and column name concerned in your rule, respectively.
     # If there are multiple columns or table, make this sentence multiple times.
-    rule_context.push_issue(
-        table=Reviews, field=CPPreviewDate, row=failing_indices
-    )
+    rule_context.push_issue(table=Reviews, field=CPPreviewDate, row=failing_indices)
 
 
 def test_validate():
