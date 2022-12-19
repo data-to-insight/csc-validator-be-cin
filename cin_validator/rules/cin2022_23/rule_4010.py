243bfcc9fc8ff5cf6bac6f3a66957446b843c939
William Levack Payne <108789355+willLPD2I@users.noreply.github.com>
2022-12-19 11:21:04 +0000

    formatting

--- cin_validator/rules/cin2022_23/rule_4010.py
+++ cin_validator/rules/cin2022_23/rule_4010.py
@@ -34,46 +34,63 @@ def validate(
     df = data_container[CINplanDates]
     df_ref = data_container[Header]
 
-    # Allocate to the start and end of the census period to collection_start and collection_end. 
+    # Allocate to the start and end of the census period to collection_start and collection_end.
     # Collection_end to be referenced as reference_date to match CIN Census terminology
     ref_data_series = df_ref[ReferenceDate]
     collection_start, reference_date = make_census_period(ref_data_series)
-       
+
     # implement rule logic as described by the Github issue. Put the description as a comment above the implementation as shown.
     # Where a <CINPlanDates> module is present, <CINPlanStartDate> (N00689) must be present and on or before the <ReferenceDate> (N00603)
-    # condition states that there must be a value in CINPlanStartDate and that value must be after the reference_date. 
+    # condition states that there must be a value in CINPlanStartDate and that value must be after the reference_date.
     # As such any NULL values for CINPlanStartDate wil be flagged with the error message
 
     df = df[(df[CINPlanStartDate].isna() | (df[CINPlanStartDate] > reference_date))]
-
     failing_indices = df.index
 
     rule_context.push_issue(
         table=CINplanDates, field=CINPlanStartDate, row=failing_indices
     )
 
+
 def test_validate():
 
     cin_start = pd.to_datetime(
-    # Create some sample data such that some values pass the validation and some fail.
+        # Create some sample data such that some values pass the validation and some fail.
         [
-                "2022-04-25", #Fail - CINPlanStartDate is after the reference_date of 31st March 2022
-                "2022-03-01", #Pass - CINPlanStartDate is before the reference_date of 31st March 2022
-                "2022-12-25", #Fail - CINPlanStartDate is after the reference_date of 31st March 2022
-                "2021-04-27", #Pass - CINPlanStartDate is before the reference_date of 31st March 2022
-                "2021-11-21", #Pass - CINPlanStartDate is before the reference_date of 31st March 2022
-                "2021-08-20", #Pass - CINPlanStartDate is before the reference_date of 31st March 2022 
-                "2020-04-17", #Pass - CINPlanStartDate is before the reference_date of 31st March 2022
-                "1999-01-30", #Pass - CINPlanStartDate is before the reference_date of 31st March 2022
-                pd.NA, #Fail - CINPlanStartDate is blank and is therefore missing. 
+            "2022-04-25",  # Fail - CINPlanStartDate is after the reference_date of 31st March 2022
+            "2022-03-01",  # Pass - CINPlanStartDate is before the reference_date of 31st March 2022
+            "2022-12-25",  # Fail - CINPlanStartDate is after the reference_date of 31st March 2022
+            "2021-04-27",  # Pass - CINPlanStartDate is before the reference_date of 31st March 2022
+            "2021-11-21",  # Pass - CINPlanStartDate is before the reference_date of 31st March 2022
+            "2021-08-20",  # Pass - CINPlanStartDate is before the reference_date of 31st March 2022
+            "2020-04-17",  # Pass - CINPlanStartDate is before the reference_date of 31st March 2022
+            "1999-01-30",  # Pass - CINPlanStartDate is before the reference_date of 31st March 2022
+            pd.NA,  # Fail - CINPlanStartDate is blank and is therefore missing.
         ],
         format="%Y/%m/%d",
         errors="coerce",
     )
+    CINids = [
+        "CINID1",
+        "CINID2",
+        "CINID3",
+        "CINID4",
+        "CINID5",
+        "CINID6",
+        "CINID7",
+        "CINID8",
+        "CINID9",
+    ]
 
-    fake_cin_start = pd.DataFrame({CINPlanStartDate: cin_start})
-    fake_header = pd.DataFrame([{ReferenceDate: "31/03/2022"}])  # the reference date will be 31/03/2022 
-    
+    fake_cin_start = pd.DataFrame({CINPlanStartDate: cin_start, "CINdetailsID": CINids})
+    fake_header = pd.DataFrame(
+        [{ReferenceDate: "31/03/2022"}]
+    )  # the reference date will be 31/03/2022
+
+    fake_header["ReferenceDate"] = pd.to_datetime(
+        fake_header["ReferenceDate"], format="%d/%m/%Y"
+    )
+
     # Run rule function passing in our sample data
     result = run_rule(validate, {CINplanDates: fake_cin_start, Header: fake_header})
 
@@ -88,7 +105,7 @@ def test_validate():
         IssueLocator(CINTable.CINplanDates, CINPlanStartDate, 0),
         IssueLocator(CINTable.CINplanDates, CINPlanStartDate, 2),
         IssueLocator(CINTable.CINplanDates, CINPlanStartDate, 8),
-     ]
+    ]
 
     # Check that the rule definition is what you wrote in the context above.
 
@@ -97,4 +114,4 @@ def test_validate():
     assert (
         result.definition.message
         == "CIN Plan start date is missing or out of data collection period"
-           )
+    )
