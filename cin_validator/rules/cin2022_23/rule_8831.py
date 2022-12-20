6917bb184a3610ebc327edf676c250bfe9f8735b
tab1tha <tambetachere@gmail.com>
2022-12-20 17:28:47 +0000

    remove ChildProtectionPlans table. independent compare

--- cin_validator/rules/cin2022_23/rule_8831.py
+++ cin_validator/rules/cin2022_23/rule_8831.py
@@ -4,11 +4,9 @@ import pandas as pd
 
 from cin_validator.rule_engine import CINTable, RuleContext, rule_definition
 from cin_validator.test_engine import run_rule
-from cin_validator.utils import make_census_period
 
 # Get tables and columns of interest from the CINTable object defined in rule_engine/__api.py
 
-ChildProtectionPlans = CINTable.ChildProtectionPlans
 CINdetails = CINTable.CINdetails
 Section47 = CINTable.Section47
 Assessments = CINTable.Assessments
@@ -19,15 +17,14 @@ S47ActualStartDate = Section47.S47ActualStartDate
 ReferralNFA = CINdetails.ReferralNFA
 DateOfInitialCPC = CINdetails.DateOfInitialCPC
 
-LAchildID = ChildProtectionPlans.LAchildID
-CINdetailsID = ChildProtectionPlans.CINdetailsID
-
+LAchildID = CINdetails.LAchildID
+CINdetailsID = CINdetails.CINdetailsID
 
 # define characteristics of rule
 @rule_definition(
-    # write the rule code here, in place of 2885
+    # write the rule code here, in place of 8831
     code=8831,
-    # replace ChildProtectionPlans with the value in the module column of the excel sheet corresponding to this rule .
+    # replace CINdetails with the value in the module column of the excel sheet corresponding to this rule .
     # Note that even if multiple tables are involved, one table will be named in the module column.
     module=CINTable.CINdetails,
     # replace the message with the corresponding value for this rule, gotten from the excel sheet.
@@ -38,7 +35,7 @@ CINdetailsID = ChildProtectionPlans.CINdetailsID
         AssessmentAuthorisationDate,
         S47ActualStartDate,
         DateOfInitialCPC,
-    ],  # TODO How can we indicate that the DateOfInitialCPC comes from both tables. Is it necessary?
+    ],
 )
 def validate(
     data_container: Mapping[CINTable, pd.DataFrame], rule_context: RuleContext
@@ -49,18 +46,15 @@ def validate(
     df_ass = data_container[Assessments].copy()
     df_47 = data_container[Section47].copy()
     df_cin = data_container[CINdetails].copy()
-    df_cpp = data_container[ChildProtectionPlans].copy()
 
     # Before you begin, rename the index so that the initial row positions can be kept intact.
-    df_cpp.index.name = "ROW_ID"
     df_47.index.name = "ROW_ID"
     df_cin.index.name = "ROW_ID"
     df_ass.index.name = "ROW_ID"
 
     # Resetting the index causes the ROW_IDs to become columns of their respective DataFrames
     # so that they can come along when the merge is done.
-    # TODO summarise with a for loop? e.g for df in [df_cpp, df_47, df_cin]
-    df_cpp.reset_index(inplace=True)
+    # TODO summarise with a for loop? e.g for df in [df_ass, df_47, df_cin]
     df_47.reset_index(inplace=True)
     df_cin.reset_index(inplace=True)
     df_ass.reset_index(inplace=True)
@@ -74,98 +68,63 @@ def validate(
     trueor1 = ["true", "1"]
     df_cin = df_cin[df_cin[ReferralNFA].isin(trueor1)]
 
-    df_cin_cpp = df_cin.merge(
-        df_cpp, on=["LAchildID", "CINdetailsID"], how="left", suffixes=["_cin", "_cpp"]
-    )
-
+    # Check columns in Section47 table
     df_cin_47 = df_cin.merge(
         df_47, on=["LAchildID", "CINdetailsID"], how="left", suffixes=["_cin", "_47"]
     )
+    # filter out rows that have an S47ActualStartDate or DateOfInitialCPC
+    condition_1 = (
+        df_cin_47[DateOfInitialCPC].notna() | df_cin_47[S47ActualStartDate].notna()
+    )
+    df_cin_47 = df_cin_47[condition_1]
+    df_cin_47["ERROR_ID"] = tuple(zip(df_cin_47[LAchildID], df_cin_47[CINdetailsID]))
+    df_47_issues = (
+        df_47.merge(df_cin_47, left_on="ROW_ID", right_on="ROW_ID_47")
+        .groupby("ERROR_ID", group_keys=False)["ROW_ID"]
+        .apply(list)
+        .reset_index()
+    )
+    cin_issues_47 = (
+        df_cin.merge(df_cin_47, left_on="ROW_ID", right_on="ROW_ID_cin")
+        .groupby("ERROR_ID", group_keys=False)["ROW_ID"]
+        .apply(list)
+        .reset_index()
+    )
 
+    # Check columns in Assessments table
     df_cin_ass = df_cin.merge(
         df_ass,
         on=["LAchildID", "CINdetailsID"],
         how="left",
         suffixes=["_cin", "_ass"],
     )
-
-    df_cin_cpp_47 = df_cin_cpp.merge(
-        df_cin_47,
-        on=[
-            "LAchildID",
-            "CINdetailsID",
-            "ROW_ID_cin",
-            "DateOfInitialCPC",
-            "ReferralNFA",
-        ],
-        how="left",
-        suffixes=["", ""],
-    )
-    merged_df = df_cin_cpp_47.merge(
-        df_cin_ass,
-        on=[
-            "LAchildID",
-            "CINdetailsID",
-            "ROW_ID_cin",
-            "DateOfInitialCPC",
-            "ReferralNFA",
-        ],
-        how="left",
-        suffixes=["", ""],
-    )
-
-    # Logical conditions - other than this, if the tables can merge, it means there's modules and they are in error
-
-    condition_1 = (
-        merged_df["AssessmentActualStartDate"].notna()
-        | merged_df["AssessmentAuthorisationDate"].notna()
-    )
-    condition_2 = merged_df[DateOfInitialCPC].notna()
-    condition_3 = merged_df["S47ActualStartDate"].notna()
-
-    merged_df = merged_df[condition_1 | condition_2 | condition_3].reset_index()
-
-    # create an identifier for each error instance.
-    # In this case, the rule is checked for each CPPstartDate, in each CINplanDates group (differentiated by CINdetailsID), in each child (differentiated by LAchildID)
-    # So, a combination of LAchildID, CINdetailsID and CPPstartDate identifies and error instance.
-    # You could also consider that CPPstartDate, unlike DateOfInitialCPC, is the leading column against which columns from the other tables are compared. So it is included in the zip.
-    merged_df["ERROR_ID"] = tuple(zip(merged_df[LAchildID], merged_df[CINdetailsID]))
-
-    # The merges were done on copies of df_cpp, df_47 and df_cin so that the column names in dataframes themselves aren't affected by the suffixes.
-    # we can now map the suffixes columns to their corresponding source tables such that the failing ROW_IDs and ERROR_IDs exist per table.
-    df_cpp_issues = (
-        df_cpp.merge(merged_df, left_on="ROW_ID", right_on="ROW_ID_cpp")
-        .groupby("ERROR_ID", group_keys=False)["ROW_ID"]
-        .apply(list)
-        .reset_index()
-    )
-    df_47_issues = (
-        df_47.merge(merged_df, left_on="ROW_ID", right_on="ROW_ID_47")
-        .groupby("ERROR_ID", group_keys=False)["ROW_ID"]
-        .apply(list)
-        .reset_index()
-    )
-    df_cin_issues = (
-        df_cin.merge(merged_df, left_on="ROW_ID", right_on="ROW_ID_cin")
-        .groupby("ERROR_ID", group_keys=False)["ROW_ID"]
-        .apply(list)
-        .reset_index()
-    )
-    print(df_cin_issues)
+    # filter out rows that have an AssessmentActualStartDate or AssessmentAuthorisationDate
+    condition_2 = df_cin_ass[AssessmentActualStartDate].notna()
+    condition_3 = df_cin_ass[AssessmentAuthorisationDate].notna()
+    df_cin_ass = df_cin_ass[condition_2 | condition_3]
+    df_cin_ass["ERROR_ID"] = tuple(zip(df_cin_ass[LAchildID], df_cin_ass[CINdetailsID]))
     df_ass_issues = (
-        df_ass.merge(merged_df, left_on="ROW_ID", right_on="ROW_ID_ass")
+        df_ass.merge(df_cin_ass, left_on="ROW_ID", right_on="ROW_ID_ass")
         .groupby("ERROR_ID", group_keys=False)["ROW_ID"]
         .apply(list)
         .reset_index()
     )
-    # Ensure that you maintain the ROW_ID, and ERROR_ID column names which are shown above. They are keywords in this project.
-    rule_context.push_type_2(
-        table=ChildProtectionPlans, columns=[LAchildID], row_df=df_cpp_issues
+    cin_issues_ass = (
+        df_cin.merge(df_cin_ass, left_on="ROW_ID", right_on="ROW_ID_cin")
+        .groupby("ERROR_ID", group_keys=False)["ROW_ID"]
+        .apply(list)
+        .reset_index()
     )
+
+    df_cin_issues = pd.concat([cin_issues_47, cin_issues_ass])
+    # in case a value was flagged in both table combinations, it'll exist twice so deduplicate df_cin_issues
+    df_cin_issues.drop_duplicates("ERROR_ID")
+    df_cin_issues.reset_index(inplace=True, drop=True)
+
+    # Ensure that you maintain the ROW_ID, and ERROR_ID column names which are shown above. They are keywords in this project.
     rule_context.push_type_2(
         table=CINdetails, columns=[DateOfInitialCPC], row_df=df_cin_issues
     )
-
     rule_context.push_type_2(
         table=Assessments, columns=[LAchildID], row_df=df_ass_issues
     )
@@ -174,45 +133,6 @@ def validate(
 
 def test_validate():
     # Create some sample data such that some values pass the validation and some fail.
-    sample_cpp = pd.DataFrame(
-        [
-            {
-                "LAchildID": "child1",
-                "CPPstartDate": "26/05/2000",
-                "CINdetailsID": "cinID10",
-            },
-            {
-                "LAchildID": "child1",
-                "CPPstartDate": "27/06/2002",
-                "CINdetailsID": "cinID2",
-            },
-            {
-                "LAchildID": "child3",
-                "CPPstartDate": "26/05/2000",
-                "CINdetailsID": "cinID3",
-            },
-            {
-                "LAchildID": "child30",
-                "CPPstartDate": "26/05/2000",
-                "CINdetailsID": "cinID3",
-            },
-            {
-                "LAchildID": "child30",
-                "CPPstartDate": pd.NA,
-                "CINdetailsID": "cinID2",
-            },
-            {
-                "LAchildID": "child3",
-                "CPPstartDate": "07/02/2001",
-                "CINdetailsID": "cinID30",
-            },
-            {
-                "LAchildID": "child3",
-                "CPPstartDate": "14/03/2001",
-                "CINdetailsID": "cinID4",
-            },
-        ]
-    )
     sample_section47 = pd.DataFrame(
         [
             {
@@ -249,19 +169,19 @@ def test_validate():
     sample_cin_details = pd.DataFrame(
         [
             {
-                "LAchildID": "child1",
+                "LAchildID": "child1",  # fail: has AssessmentAuthorisationDate
                 "DateOfInitialCPC": pd.NA,
                 "CINdetailsID": "cinID1",
                 "ReferralNFA": "1",
             },
             {
-                "LAchildID": "child2",
+                "LAchildID": "child2",  # fail: has S47ActualStartDate
                 "DateOfInitialCPC": pd.NA,
                 "CINdetailsID": "cinID2",
                 "ReferralNFA": "true",
             },
             {
-                "LAchildID": "child3",
+                "LAchildID": "child3",  # fail: has AssessmentActualStartDate
                 "DateOfInitialCPC": pd.NA,
                 "CINdetailsID": "cinID3",
                 "ReferralNFA": "1",
@@ -276,19 +196,19 @@ def test_validate():
                 "LAchildID": "child3",
                 "DateOfInitialCPC": "26/05/2000",
                 "CINdetailsID": "cinID2",
-                "ReferralNFA": "false",
+                "ReferralNFA": "false",  # ignore
             },
             {
                 "LAchildID": "child3",
                 "DateOfInitialCPC": "26/05/2003",
                 "CINdetailsID": "cinID8",
-                "ReferralNFA": "false",
+                "ReferralNFA": "false",  # ignore
             },
             {  # 6 pass
                 "LAchildID": "child3",
                 "DateOfInitialCPC": "14/03/2001",
                 "CINdetailsID": "cinID4",
-                "ReferralNFA": "false",
+                "ReferralNFA": "false",  # ignore
             },
         ]
     )
@@ -298,7 +218,7 @@ def test_validate():
                 "LAchildID": "child1",  # fails for having a module
                 "CINdetailsID": "cinID1",
                 "AssessmentAuthorisationDate": "01/01/2000",
-                "AssessmentActualStartDate": "01/01/2000",
+                "AssessmentActualStartDate": pd.NA,
             },
             {
                 "LAchildID": "child3",  # fails for having a module
@@ -345,7 +265,6 @@ def test_validate():
     result = run_rule(
         validate,
         {
-            ChildProtectionPlans: sample_cpp,
             Section47: sample_section47,
             CINdetails: sample_cin_details,
             Assessments: sample_ass,
@@ -354,10 +273,10 @@ def test_validate():
 
     # Use .type2_issues to check for the result of .push_type2_issues() which you used above.
     issues_list = result.type2_issues
-    assert len(issues_list) == 4
+    assert len(issues_list) == 3
     # the function returns a list on NamedTuples where each NamedTuple contains (table, column_list, df_issues)
     # pick any table and check it's values. the tuple in location 1 will contain the Section47 columns because that's the second thing pushed above.
-    issues = issues_list[1]
+    issues = issues_list[0]
 
     # get table name and check it. Replace Section47 with the name of your table.
     issue_table = issues.table
@@ -384,13 +303,6 @@ def test_validate():
         [
             {
                 "ERROR_ID": (
-                    "child1",  # ChildID
-                    "cinID1",  # CINdetailsID
-                ),
-                "ROW_ID": [0],
-            },
-            {
-                "ERROR_ID": (
                     "child2",  # ChildID
                     "cinID2",  # CINdetailsID
                 ),
@@ -398,17 +310,24 @@ def test_validate():
             },
             {
                 "ERROR_ID": (
-                    "child3",  # ChildID
-                    "cinID3",  # CINdetailsID
-                ),
-                "ROW_ID": [2],
-            },
-            {
-                "ERROR_ID": (
                     "child4",  # ChildID
                     "cinID4",  # CINdetailsID
                 ),
                 "ROW_ID": [3],
+            },
+            {
+                "ERROR_ID": (
+                    "child1",  # ChildID
+                    "cinID1",  # CINdetailsID
+                ),
+                "ROW_ID": [0],
+            },
+            {
+                "ERROR_ID": (
+                    "child3",  # ChildID
+                    "cinID3",  # CINdetailsID
+                ),
+                "ROW_ID": [2],
             },
         ]
     )
