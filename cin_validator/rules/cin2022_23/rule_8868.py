c0d4c67a5ac1d198d56137dd1243c3e8330f7ab5
tab1tha <tambetachere@gmail.com>
2022-12-21 10:37:35 +0000

    update comments

--- cin_validator/rules/cin2022_23/rule_8868.py
+++ cin_validator/rules/cin2022_23/rule_8868.py
@@ -4,27 +4,22 @@ import pandas as pd
 
 from cin_validator.rule_engine import CINTable, RuleContext, rule_definition
 from cin_validator.test_engine import run_rule
-from cin_validator.utils import make_census_period
 
 # Get tables and columns of interest from the CINTable object defined in rule_engine/__api.py
-
 
 CINdetails = CINTable.CINdetails
 Section47 = CINTable.Section47
 
 CINclosureDate = CINdetails.CINclosureDate
-# TODO is it necessary to get LAchildID per table involved?
 LAchildID = CINdetails.LAchildID
 CINdetailsID = CINdetails.CINdetailsID
-# TODO is it redundant to have both of them here?
 DateOfInitialCPC = Section47.DateOfInitialCPC
 ICPCnotRequired = Section47.ICPCnotRequired
 
 # define characteristics of rule
 @rule_definition(
-    # write the rule code here, in place of 2885
     code=8868,
-    # replace ChildProtectionPlans with the value in the module column of the excel sheet corresponding to this rule .
+    # replace CINdetails with the value in the module column of the excel sheet corresponding to this rule .
     # Note that even if multiple tables are involved, one table will be named in the module column.
     module=CINTable.CINdetails,
     # replace the message with the corresponding value for this rule, gotten from the excel sheet.
@@ -34,14 +29,13 @@ ICPCnotRequired = Section47.ICPCnotRequired
         ICPCnotRequired,
         CINclosureDate,
         DateOfInitialCPC,
-    ],  # TODO How can we indicate that the DateOfInitialCPC comes from both tables. Is it necessary?
+    ],
 )
 def validate(
     data_container: Mapping[CINTable, pd.DataFrame], rule_context: RuleContext
 ):
     # PREPARING DATA
 
-    # Replace ChildProtectionPlans with the name of the table you need.
     df_47 = data_container[Section47].copy()
     df_cin = data_container[CINdetails].copy()
 
@@ -58,7 +52,6 @@ def validate(
 
     CINclosure_present = df_cin[df_cin[CINclosureDate].notna()]
 
-    # Create a dataframe that contains all the cin_details and cpp information for children whose cppstartdate exists and is within period.
     merged_df = CINclosure_present.merge(
         df_47,
         on=[LAchildID, "CINdetailsID"],
@@ -71,20 +64,12 @@ def validate(
         ~merged_df[ICPCnotRequired].isin(oneortrue)
     )
 
-    # If the rule means that CINdetails' value should also be checked *only if* Section47's value is NaN, then
-    # condition = (merged_df[CPPstartDate] != merged_df[DateOfInitialCPC_47]) & merged_df[DateOfInitialCPC_47].notna()
-    #                | (merged_df[DateOfInitialCPC_47].isna()&(merged_df[CPPstartDate] != merged_df[DateOfInitialCPC_cin]))
-
     # get all the data that fits the failing condition.
     merged_df = merged_df[condition].reset_index()
 
     # create an identifier for each error instance.
-    # In this case, the rule is checked for each CPPstartDate, in each CINplanDates group (differentiated by CINdetailsID), in each child (differentiated by LAchildID)
-    # So, a combination of LAchildID, CINdetailsID and CPPstartDate identifies and error instance.
-    # You could also consider that CPPstartDate, unlike DateOfInitialCPC, is the leading column against which columns from the other tables are compared. So it is included in the zip.
     merged_df["ERROR_ID"] = tuple(zip(merged_df[LAchildID], merged_df[CINdetailsID]))
 
-    # The merges were done on copies of df_cpp, df_47 and df_cin so that the column names in dataframes themselves aren't affected by the suffixes.
     # we can now map the suffixes columns to their corresponding source tables such that the failing ROW_IDs and ERROR_IDs exist per table.
     df_47_issues = (
         df_47.merge(merged_df, left_on="ROW_ID", right_on="ROW_ID_47")
@@ -219,20 +204,20 @@ def test_validate():
     issues_list = result.type2_issues
     assert len(issues_list) == 2
     # the function returns a list on NamedTuples where each NamedTuple contains (table, column_list, df_issues)
-    # pick any table and check it's values. the tuple in location 1 will contain the Section47 columns because that's the second thing pushed above.
+    # pick any table and check it's values. the tuple in location 1 will contain the CINdetails columns because that's the second thing pushed above.
     issues = issues_list[1]
 
-    # get table name and check it. Replace Section47 with the name of your table.
+    # get table name and check it. Replace CINdetails with the name of your table.
     issue_table = issues.table
     assert issue_table == CINdetails
 
-    # check that the right columns were returned. Replace DateOfInitialCPC  with a list of your columns.
+    # check that the right columns were returned. Replace CINclosureDate with a list of your columns.
     issue_columns = issues.columns
     assert issue_columns == [CINclosureDate]
 
     # check that the location linking dataframe was formed properly.
     issue_rows = issues.row_df
-    # replace 2 with the number of failing points you expect from the sample data.
+    # replace 1 with the number of failing points you expect from the sample data.
     assert len(issue_rows) == 1
     # check that the failing locations are contained in a DataFrame having the appropriate columns. These lines do not change.
     assert isinstance(issue_rows, pd.DataFrame)
@@ -258,7 +243,7 @@ def test_validate():
 
     # Check that the rule definition is what you wrote in the context above.
 
-    # replace 2885 with the rule code and put the appropriate message in its place too.
+    # replace 8868 with the rule code and put the appropriate message in its place too.
     assert result.definition.code == 8868
     assert (
         result.definition.message
