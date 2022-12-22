43c836c1973eb77023da09c5455a1108af7d71ca
tab1tha <tambetachere@gmail.com>
2022-12-22 18:15:04 +0000

    refactor. compare within child

--- cin_validator/rules/cin2022_23/rule_8816.py
+++ cin_validator/rules/cin2022_23/rule_8816.py
@@ -4,7 +4,6 @@ import pandas as pd
 
 from cin_validator.rule_engine import CINTable, RuleContext, rule_definition
 from cin_validator.test_engine import run_rule
-from cin_validator.utils import make_census_period
 
 # Get tables and columns of interest from the CINTable object defined in rule_engine/__api.py
 
@@ -20,7 +19,7 @@ CINclosureDate = CINdetails.CINclosureDate
 @rule_definition(
     # write the rule code here
     code=8816,
-    # replace ChildProtectionPlans with the value in the module column of the excel sheet corresponding to this rule .
+    # replace CINdetails with the value in the module column of the excel sheet corresponding to this rule .
     # Note that even if multiple tables are involved, one table will be named in the module column.
     module=CINTable.CINdetails,
     # replace the message with the corresponding value for this rule, gotten from the excel sheet.
@@ -36,63 +35,63 @@ def validate(
 ):
     # PREPARING DATA
 
-    # Replace ChildProtectionPlans with the name of the table you need.
     df_cin = data_container[CINdetails].copy()
 
     # Before you begin, rename the index so that the initial row positions can be kept intact.
     df_cin.index.name = "ROW_ID"
 
-    # Resetting the index causes the ROW_IDs to become columns of their respective DataFrames
-    # so that they can come along when the merge is done.
-    df_cin.reset_index(inplace=True)
-
     # If there is a <CINdetails> module present for the child where:
     # <CINclosureDate> (N00102) is missing; and
     # <ReferralNFA> (N00112) = false or 0
     # then the <CINreferralDate> (N00100) for this module must be the latest of all Referral Dates for that child.
-    df_cin2 = df_cin.copy()
+
+    # df_cin2 = df_cin.copy()
+    # find out the latest referral date (and its index position) for each child and attach it to all the rows for that child.
+    df_cin["latest_referral"] = df_cin.groupby(LAchildID)[CINreferralDate].transform(
+        "max"
+    )
+    df_cin["latest_referral_ind"] = df_cin.groupby(LAchildID)[
+        CINreferralDate
+    ].transform("idxmax")
+
+    # resetting the index makes ROW_ID into a column to keep it intact. The logic can now begin.
+    df_cin.reset_index(inplace=True)
+
+    # filter the rows of interest
     falseorzero = ["false", "0"]
     df_cin = df_cin[
         (df_cin[CINclosureDate].isna()) & (df_cin[ReferralNFA].isin(falseorzero))
     ]
+    # check those that do not meet the requirements
+    condition = df_cin[CINreferralDate] < df_cin["latest_referral"]
+    df_cin = df_cin[condition]
 
-    #  Merge tables to test for overlaps
-    df_merged = df_cin.merge(
-        df_cin2,
-        on=["LAchildID"],
-        how="left",
-        suffixes=("_cin", "_cin2"),
-    )
-
-    # Exclude rows where the CPPID is the same on both sides
-    df_merged = df_merged[
-        (df_merged["CINdetailsID_cin"] != df_merged["CINdetailsID_cin2"])
-    ]
-
-    df_merged = df_merged[
-        df_merged["CINreferralDate_cin"] < df_merged["CINreferralDate_cin2"]
-    ].reset_index()
-
-    df_merged["ERROR_ID"] = tuple(
-        zip(
-            df_merged[LAchildID],
-            df_merged["CINdetailsID_cin"],
-            df_merged["CINreferralDate_cin"],
-        )
+    df_cin["ERROR_ID"] = tuple(
+        zip(df_cin[LAchildID], df_cin[CINdetailsID], df_cin[CINreferralDate])
     )
 
     df_cin_issues = (
-        df_cin.merge(df_merged, left_on="ROW_ID", right_on="ROW_ID_cin")
-        .groupby("ERROR_ID", group_keys=False)["ROW_ID"]
+        df_cin.groupby("ERROR_ID", group_keys=False)["ROW_ID"].apply(list).reset_index()
+    )
+    # the locations of the latest_referral date should also be flagged
+    df_cin_issues_cause = (
+        df_cin.groupby("ERROR_ID", group_keys=False)["latest_referral_ind"]
         .apply(list)
         .reset_index()
     )
+    # rename column to fit expected structure of a df_issues dataframe
+    df_cin_issues_cause.rename(columns={"latest_referral_ind": "ROW_ID"}, inplace=True)
 
     # Ensure that you maintain the ROW_ID, and ERROR_ID column names which are shown above. They are keywords in this project.
     rule_context.push_type_3(
         table=CINdetails,
-        columns=[CINclosureDate],
+        columns=[CINreferralDate],
         row_df=df_cin_issues,
+    )
+    rule_context.push_type_3(
+        table=CINdetails,
+        columns=[CINreferralDate],
+        row_df=df_cin_issues_cause,
     )
 
 
@@ -102,31 +101,46 @@ def test_validate():
     sample_cin = pd.DataFrame(
         [
             {
-                "LAchildID": "child1",  # fail
+                "LAchildID": "child1",
                 "CINclosureDate": pd.NA,
-                "CINreferralDate": "26/10/2000",
+                "CINreferralDate": "26/10/2000",  # 0 fail: not latest referral in child1
                 "CINdetailsID": "cinID1",
                 "ReferralNFA": "0",
             },
             {
-                "LAchildID": "child1",  # 0 Pass
+                "LAchildID": "child1",
                 "CINclosureDate": "26/05/2001",
-                "CINreferralDate": "26/10/2001",
+                "CINreferralDate": "26/10/2001",  # 1 ignore: CINclosureDate notna
                 "CINdetailsID": "cinID2",
                 "ReferralNFA": "0",
             },
             {
-                "LAchildID": "child1",  # fail
+                "LAchildID": "child1",
                 "CINclosureDate": pd.NA,
-                "CINreferralDate": "26/10/1999",
+                "CINreferralDate": "26/10/1999",  # 2 fail: not latest referral in child1
                 "CINdetailsID": "cinID3",
                 "ReferralNFA": "0",
             },
             {
-                "LAchildID": "child1",  # Pass
+                "LAchildID": "child1",
                 "CINclosureDate": pd.NA,
-                "CINreferralDate": "26/10/2002",
+                "CINreferralDate": "26/10/2002",  # 3 pass: latest referral in child1
                 "CINdetailsID": "cinID4",
+                "ReferralNFA": "0",
+            },
+            # child2
+            {
+                "LAchildID": "child2",
+                "CINclosureDate": pd.NA,
+                "CINreferralDate": "26/10/1999",  # 4 pass: latest referral in child2
+                "CINdetailsID": "cinID3",
+                "ReferralNFA": "0",
+            },
+            {
+                "LAchildID": "child2",
+                "CINclosureDate": pd.NA,
+                "CINreferralDate": "26/10/1990",  # 5 fail: not latest referral in child2
+                "CINdetailsID": "cinID3",
                 "ReferralNFA": "0",
             },
         ]
@@ -150,7 +164,7 @@ def test_validate():
 
     # Use .type2_issues to check for the result of .push_type2_issues() which you used above.
     issues_list = result.type3_issues
-    assert len(issues_list) == 1
+    assert len(issues_list) == 2
     # the function returns a list on NamedTuples where each NamedTuple contains (table, column_list, df_issues)
     # pick any table and check it's values. the tuple in location 1 will contain the Reviews columns because that's the second thing pushed above.
     issues = issues_list[0]
@@ -161,12 +175,12 @@ def test_validate():
 
     # check that the right columns were returned. Replace CPPreviewDate  with a list of your columns.
     issue_columns = issues.columns
-    assert issue_columns == [CINclosureDate]
+    assert issue_columns == [CINreferralDate]
 
     # check that the location linking dataframe was formed properly.
     issue_rows = issues.row_df
     # replace 3 with the number of failing points you expect from the sample data.
-    assert len(issue_rows) == 2
+    assert len(issue_rows) == 3
 
     # check that the failing locations are contained in a DataFrame having the appropriate columns. These lines do not change.
     assert isinstance(issue_rows, pd.DataFrame)
@@ -185,7 +199,7 @@ def test_validate():
                     "cinID1",
                     pd.to_datetime("26/10/2000", format="%d/%m/%Y"),
                 ),
-                "ROW_ID": [0, 0],
+                "ROW_ID": [0],
             },
             {
                 "ERROR_ID": (
@@ -193,7 +207,15 @@ def test_validate():
                     "cinID3",
                     pd.to_datetime("26/10/1999", format="%d/%m/%Y"),
                 ),
-                "ROW_ID": [2, 2, 2],
+                "ROW_ID": [2],
+            },
+            {
+                "ERROR_ID": (
+                    "child2",
+                    "cinID3",
+                    pd.to_datetime("26/10/1990", format="%d/%m/%Y"),
+                ),
+                "ROW_ID": [5],
             },
         ]
     )
