ba6b26042262b682debe137abd8916c72b0a5d10
tab1tha <tambetachere@gmail.com>
2022-12-22 08:01:35 +0000

    correct file type

--- cin_validator/rules/cin2022_23/rule_2886Q.py
+++ cin_validator/rules/cin2022_23/rule_2886Q.py
@@ -1,4 +1,3 @@
-
 from typing import Mapping
 
 import pandas as pd
@@ -23,15 +22,15 @@ def validate(
 ):
     df = data_container[ChildIdentifiers]
 
-    #The sum of: the number of child records where <GenderCurrent> (N00065) is equal to zero or missing, 
-    # and the <ExpectedPersonBirthDate> (N00098) is equal to missing, divided by the total number of 
+    # The sum of: the number of child records where <GenderCurrent> (N00065) is equal to zero or missing,
+    # and the <ExpectedPersonBirthDate> (N00098) is equal to missing, divided by the total number of
     # child records should be less than or equal to 0.02. Validation should be triggered at LA level.
 
     # get the total number of child records
     num_records = len(df)
 
     # get the number of child records that fit the specified condition.
-    missing_gender = df[df[GenderCurrent].isna() | df[GenderCurrent]==0]
+    missing_gender = df[df[GenderCurrent].isna() | df[GenderCurrent] == 0]
     missing_date = df[df[ExpectedPersonBirthDate].isna()]
     condition = missing_gender & missing_date
     # since the filtered number has to be compared to the original, make a copy of the data.
@@ -40,31 +39,29 @@ def validate(
     num_issues = len(df_issues)
 
     # calculate
-    missing_ratio = num_issues/num_records
+    missing_ratio = num_issues / num_records
     if missing_ratio > 0.02:
-        rule_context.push_la_level(rule_context.definition.code, rule_context.definition.message)
+        rule_context.push_la_level(
+            rule_context.definition.code, rule_context.definition.message
+        )
     else:
         pass
 
 
 def test_validate():
 
-    sample_child_ids = pd.DataFrame([
-        {
-            GenderCurrent: 0, 
-            ExpectedPersonBirthDate: pd.NA
-        },
-        {
-            GenderCurrent: 1, 
-            ExpectedPersonBirthDate: pd.NA
-        },
-        {
-            GenderCurrent: pd.NA, 
-            ExpectedPersonBirthDate: pd.NA
-        },
-    ])
-    
+    sample_child_ids = pd.DataFrame(
+        [
+            {GenderCurrent: 0, ExpectedPersonBirthDate: pd.NA},
+            {GenderCurrent: 1, ExpectedPersonBirthDate: pd.NA},
+            {GenderCurrent: pd.NA, ExpectedPersonBirthDate: pd.NA},
+        ]
+    )
+
     # Run rule function passing in our sample data
     result = run_rule(validate, {ChildIdentifiers: sample_child_ids})
 
-    assert result.la_issues == ("2886Q", "Please check: Percentage of children with no gender recorded is more than 2% (excluding unborns)")
+    assert result.la_issues == (
+        "2886Q",
+        "Please check: Percentage of children with no gender recorded is more than 2% (excluding unborns)",
+    )
