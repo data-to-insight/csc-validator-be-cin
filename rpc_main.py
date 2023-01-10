f49aa8285f94c61347c2714e70e67dddae733b5e
tab1tha <tambetachere@gmail.com>
2023-01-10 09:59:25 +0000

    conversion and validation both work

--- rpc_main.py
+++ rpc_main.py
@@ -4,26 +4,24 @@ from cin_validator import cin_validator_class as cin_class
 
 app = RpcApp("validate_cin")
 
-data_files = None
-
 
 @app.call
 def generate_csv():
-    global data_files
+
     data_files = cin_class.process_data(
-        filename="fake_data\CIN_Census_2021.xml",
+        filename="fake_data\CIN_Census_2021.xml", as_dict=True
     )
-    return data_files
+    json_data_files = {
+        table_name: table_df.to_dict(orient="records")
+        for table_name, table_df in data_files.items()
+    }
+    return json_data_files
 
 
 @app.call
-def cin_validate():
-    global data_files
-    if data_files:
-        data_files = data_files
-    else:
-        data_files = cin_class.process_data(filename="fake_data\CIN_Census_2021.xml")
-
+def cin_validate(cin_data="fake_data\CIN_Census_2021.xml", data_files=None):
+    if not data_files:
+        data_files = cin_class.process_data(cin_data)
     validator = cin_class.CinValidationSession(data_files, ruleset="rules.cin2022_23")
     error_report = validator.json_issue_report
     rule_defs = validator.json_rule_descriptors
