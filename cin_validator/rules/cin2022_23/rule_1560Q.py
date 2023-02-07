e9706eaed1ea1981a7738e6ad06b39e706b20efc
William Levack Payne <108789355+willLPD2I@users.noreply.github.com>
2023-02-07 09:36:10 +0000

    1560Q noow correctly checks <FormerUPN> not <UPN>

--- cin_validator/rules/cin2022_23/rule_1560Q.py
+++ cin_validator/rules/cin2022_23/rule_1560Q.py
@@ -12,7 +12,7 @@ from cin_validator.rule_engine import (
 from cin_validator.test_engine import run_rule
 
 ChildIdentifiers = CINTable.ChildIdentifiers
-UPN = ChildIdentifiers.UPN
+FormerUPN = ChildIdentifiers.FormerUPN
 
 
 @rule_definition(
@@ -20,7 +20,7 @@ UPN = ChildIdentifiers.UPN
     module=CINTable.ChildIdentifiers,
     rule_type=RuleType.QUERY,
     message="Please check: Former UPN wrongly formatted",
-    affected_fields=[UPN],
+    affected_fields=[FormerUPN],
 )
 def validate(
     data_container: Mapping[CINTable, pd.DataFrame], rule_context: RuleContext
@@ -30,28 +30,28 @@ def validate(
     # <FormerUPN> (N00002) where present should be in the correct format, as specified in the data table
 
     #  filter rows of df where the UPN column doesn't have Na/NaN values
-    df = df.loc[df["UPN"].notna()]
+    df = df.loc[df["FormerUPN"].notna()]
 
     # Flag locations where
     # FormerUPN is not 13 characters long
-    check_length = df["UPN"].str.len() != 13
+    check_length = df["FormerUPN"].str.len() != 13
     # FormerUPN does not contain a full digit between edges.
-    digit_within = ~df["UPN"].str[1:-1].str.isdigit()
+    digit_within = ~df["FormerUPN"].str[1:-1].str.isdigit()
     # FormerUPN's edges are not characters of th alphabet
-    check_edges = (~df["UPN"].str[0].str.isalpha()) | (~df["UPN"].str[-1].str.isalpha())
+    check_edges = (~df["FormerUPN"].str[0].str.isalpha()) | (~df["FormerUPN"].str[-1].str.isalpha())
 
     failing_indices = df[check_length | digit_within | check_edges].index
 
-    rule_context.push_issue(table=ChildIdentifiers, field=UPN, row=failing_indices)
+    rule_context.push_issue(table=ChildIdentifiers, field=FormerUPN, row=failing_indices)
 
 
 def test_validate():
     child_identifiers = pd.DataFrame(
         [
-            {"UPN": pd.NA},  # 0 ignore
-            {"UPN": "X98765432123B"},  # 1 pass
-            {"UPN": "X0000y000000K"},  # 2 fail non-alphabet within
-            {"UPN": "X9872123B"},  # 3 wrong length
+            {"FormerUPN": pd.NA},  # 0 ignore
+            {"FormerUPN": "X98765432123B"},  # 1 pass
+            {"FormerUPN": "X0000y000000K"},  # 2 fail non-alphabet within
+            {"FormerUPN": "X9872123B"},  # 3 wrong length
         ]
     )
 
@@ -60,8 +60,8 @@ def test_validate():
     issues = list(result.issues)
     assert len(issues) == 2
     assert issues == [
-        IssueLocator(CINTable.ChildIdentifiers, UPN, 2),
-        IssueLocator(CINTable.ChildIdentifiers, UPN, 3),
+        IssueLocator(CINTable.ChildIdentifiers, FormerUPN, 2),
+        IssueLocator(CINTable.ChildIdentifiers, FormerUPN, 3),
     ]
 
     assert result.definition.code == "1560Q"
