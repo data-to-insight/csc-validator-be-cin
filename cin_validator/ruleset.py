661e49c3c8a988924dc6867d0c62d8ebaa5e5ebe
tab1tha <tambe.tabitha@socialfinance.org.uk>
2023-04-18 18:12:42 +0100

    include cin keyword in ruleset names

--- cin_validator/ruleset.py
+++ cin_validator/ruleset.py
@@ -9,16 +9,16 @@ ruleset_years = []
 import cin_validator.rules.cin2022_23
 
 cin22_23 = registry.to_dict()
-ruleset_updates["2022_23"] = {"deleted": [], "ruleset": cin22_23}
-ruleset_years.append("2022_23")
+ruleset_updates["cin2022_23"] = {"deleted": [], "ruleset": cin22_23}
+ruleset_years.append("cin2022_23")
 registry.reset()
 
 # 2023_24
 import cin_validator.rules.cin_2023_24
 
 cin23_24 = registry.to_dict()
-ruleset_updates["2023_24"] = {"deleted": [], "ruleset": cin23_24}
-ruleset_years.append("2023_24")
+ruleset_updates["cin2023_24"] = {"deleted": [], "ruleset": cin23_24}
+ruleset_years.append("cin2023_24")
 registry.reset()
 
 
@@ -27,15 +27,16 @@ registry.reset()
 
 def create_registry(ruleset=ruleset_years[-1]):
     """
-    :param str ruleset: year whose version of rules should be run. e.g 2022_23. Defaults to latest recorded year.
+    :param str ruleset: year whose version of rules should be run. e.g cin2022_23.
+    Defaults to latest recorded year.
     """
 
     chosen_ruleset_ind = ruleset_years.index(ruleset)
 
     # the 2022_23 ruleset will always be the starting point.
-    combined_ruleset = ruleset_updates["2022_23"]["ruleset"]
+    combined_ruleset = ruleset_updates["cin2022_23"]["ruleset"]
 
-    # loop starts from position 1 because 2022_23 is in position 0.
+    # loop starts from position 1 because cin2022_23 is in position 0.
     # offset by 1 to ensure that the chosen ruleset is included.
     for year in ruleset_years[1 : chosen_ruleset_ind + 1]:
         year_update = ruleset_updates[year]
