4bd5a70bd4dcd0267218dd7322d3463c506c6e50
Tambe Tabitha Achere <tambetachere@gmail.com>
2023-08-23 11:41:45 +0100

    Test/general tests (#331)
    * check that ruleset is full
    
    * add comments
    
    * note about why no docs for tests
    
    * update total rules to 105

---
+++ tests/test_ruleset.py
@@ -0,0 +1,8 @@
+def test_ruleset_complete():
+    # registry is filled with whatever ruleset is imported.
+    # If multiple rulesets are imported, the registry will contain the sum of them all.
+    import cin_validator.rules.cin2022_23
+    from cin_validator.rule_engine import registry
+
+    # check that there are 107 rules in the 2022/2023 version of CIN rules.
+    assert len(registry) == 105
