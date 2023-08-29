9a9606522c50d3b635e0bd5aab4d6f62b464a9d7
tab1tha <tambe.tabitha@socialfinance.org.uk>
2023-08-29 16:17:22 +0100

    remove explicit test

--- tests/rule_engine/test_registry.py
+++ tests/rule_engine/test_registry.py
@@ -1,49 +1,7 @@
-import os
-from pathlib import Path
-
 import pytest
-from testfixtures import tempdir
 
 from cin_validator.rule_engine import rule_definition
-from cin_validator.rules.ruleset_utils import (
-    check_duplicate_rules,
-    extract_validator_functions,
-)
-
-
-@tempdir()
-def test_register_rules(dir):
-    file_content = """
-    \nfrom cin_validator.rule_engine import rule_definition
-    \n@rule_definition(
-        code=8500,
-        rule_type="error",
-        module="Child Identifiers",
-        message="LA Child ID missing",
-        affected_fields=["LAchildID"],
-    )
-    \ndef validate_8500():
-        pass
-
-    \n@rule_definition(
-        code=8501,
-        rule_type="error",
-        module="Child Identifiers",
-        message="LA Child ID missing",
-        affected_fields=["LAchildID"],
-    )
-    \ndef validate_8501():
-        pass
-    """
-
-    # write the content above into a temporary file
-    with open("tests/rule_test_registry.py", "w") as f:
-        f.write(file_content)
-    path = Path(dir.path) / "tests/rule_test_registry.py"
-    registry = extract_validator_functions([path])
-
-    assert len(registry) == 2
-    os.remove("tests/rule_test_registry.py")
+from cin_validator.rules.ruleset_utils import check_duplicate_rules
 
 
 def test_register_duplicate_code_raises_error():
