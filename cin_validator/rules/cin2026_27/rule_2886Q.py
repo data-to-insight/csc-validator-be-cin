c51ed006355dad9cc46a9a37cfeba33d7a0fad4a
William Levack Payne <108789355+WillLP-code@users.noreply.github.com>
2026-01-05 15:16:11 +0000

    adds rule 8995Q

--- cin_validator/rules/cin2026_27/rule_2886Q.py
+++ cin_validator/rules/cin2026_27/rule_2886Q.py
@@ -5,43 +5,22 @@ import pandas as pd
 from cin_validator.rule_engine import CINTable, RuleContext, RuleType, rule_definition
 from cin_validator.test_engine import run_rule
 
-ChildIdentifiers = CINTable.PreProceedings
-GenderCurrent = ChildIdentifiers.GenderCurrent
-ExpectedPersonBirthDate = ChildIdentifiers.ExpectedPersonBirthDate
-
+PreProceedings = CINTable.PreProceedings
 
 # define characteristics of rule
 @rule_definition(
     code="8995Q",
     rule_type=RuleType.QUERY,
-    module=CINTable.ChildIdentifiers,
+    module=CINTable.PreProceedings,
     message="No module 5 (pre-proceedings and FGM data) has been provided for any child.",
-    affected_fields=[ExpectedPersonBirthDate, ChildIdentifiers],
+    affected_fields=[],
 )
 def validate(
     data_container: Mapping[CINTable, pd.DataFrame], rule_context: RuleContext
 ):
-    df = data_container[ChildIdentifiers]
+    df = data_container[PreProceedings]
 
-    # The sum of: the number of child records where <GenderCurrent> (N00065) is equal to zero or missing,
-    # and the <ExpectedPersonBirthDate> (N00098) is equal to missing, divided by the total number of
-    # child records should be less than or equal to 0.02. Validation should be triggered at LA level.
-
-    # get the total number of child records
-    num_records = len(df)
-
-    # get the number of child records that fit the specified condition.
-    missing_gender = df[GenderCurrent].isna() | df[GenderCurrent] == 0
-    missing_date = df[ExpectedPersonBirthDate].isna()
-    condition = missing_gender & missing_date
-    # since the filtered number has to be compared to the original, make a copy of the data.
-    df_issues = df.copy()
-    df_issues = df_issues[condition]
-    num_issues = len(df_issues)
-
-    # calculate
-    missing_ratio = num_issues / num_records
-    if missing_ratio > 0.02:
+    if len(df) < 1:
         rule_context.push_la_level(
             rule_context.definition.code, rule_context.definition.message
         )
@@ -50,18 +29,12 @@ def validate(
 
 
 def test_validate():
-    sample_child_ids = pd.DataFrame(
-        [
-            {GenderCurrent: 0, ExpectedPersonBirthDate: pd.NA},
-            {GenderCurrent: 1, ExpectedPersonBirthDate: pd.NA},
-            {GenderCurrent: pd.NA, ExpectedPersonBirthDate: pd.NA},
-        ]
-    )
+    sample_pre_proceedings = pd.DataFrame()
 
     # Run rule function passing in our sample data
-    result = run_rule(validate, {ChildIdentifiers: sample_child_ids})
+    result = run_rule(validate, {PreProceedings: sample_pre_proceedings})
 
     assert result.la_issues == (
-        "2886Q",
-        "Please check and either amend or provide a reason: Percentage of children with no gender recorded is more than 2% (excluding unborns)",
+        "8995Q",
+        "No module 5 (pre-proceedings and FGM data) has been provided for any child.",
     )
